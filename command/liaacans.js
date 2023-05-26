3a95b3eb7544dd29773a9bf1a717445e19161fa9
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2023-05-26 14:03:30 +0700

    Update liaacans.js

--- command/liaacans.js
+++ command/liaacans.js
@@ -4886,10 +4886,10 @@ m.reply(String(e))
 
 // AUTO SIMI SIMI GES, KLO MW TANPA BERBICARA/NGETIK, MATIIN AJAH:V, CARANYA? /*INI*/ KAYA GITU YA!
 
-if (!text) return
+/*if (!text) return
        let api = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=id`)
         let res = await api.json()
-        m.reply(res.success)
+        m.reply(res.success)*/
 
 // END SIMI SIMI:V
 
