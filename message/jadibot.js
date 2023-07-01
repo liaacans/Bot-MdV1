cc38f5b1491c76235d6536282ec6749863f3af36
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2023-07-01 23:35:52 +0700

    Add files via upload

--- message/jadibot.js
+++ message/jadibot.js
@@ -1,90 +1,75 @@
-// Fix Aja Kalau Bisa
-const { default: makeWaSocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require('@adiwajshing/baileys')
-const logg = (pino = require("pino"));
-const { Boom } = require('@hapi/boom');
-const yargs = require('yargs/yargs')
+const { default: makeWaSocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
+const color = (text, color) => { return !color ? chalk.green(text) : chalk.keyword(color)(text) }
+const log = (pino = require("pino"));
+const { Boom } = require('@hapi/boom')
 const fs = require('fs')
-const FileType = require('file-type')
+const yargs = require('yargs/yargs')
 const chalk = require('chalk')
+const qrcode = require('qrcode')
+const FileType = require('file-type')
 const path = require('path')
-const qrcode = require('qrcode');
+const PhoneNumber = require('awesome-phonenumber')
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('../message/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('../message/myfunc')
-const storetes = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'storetes' }) })
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 
+if (global.conns instanceof Array) console.log()
+else global.conns = []
 
-if (global.listJadibot instanceof Array) console.log()
-else global.listJadibot = []
-
-const jadibot = async (liaacans, msg, from) => {
+const jadibot = async (liaacans, m, from) => {
 const { sendImage, sendMessage } = liaacans;
 const { reply, sender } = m;
-let userbot = `${liaacans.decodeJid(liaacans.user.id)}`
-const { state, saveState } = useSingleFileAuthState(`./session/jadibot/@${userbot.split("@")[0]}`)
-try {
-async function startliaacans() {
-let { version, isLatest } = await fetchLatestBaileysVersion();
-const liaacans = await makeWaSocket({
-auth: state,
-printQRInTerminal: true,
-browser: ['Bot Numpang', "Safari", "1.0.0"],
-logger: logg({ level: "silent" }),
-version,
+const { state } = useSingleFileAuthState(`./session/jadibot/${from}.json`)
+
+const connectToWhatsApp = () => {
+const liaacans = makeWaSocket({ logger: pino ({ level: 'silent' }), printQRInTerminal: true, auth: state, browser: ["Bot Numpang - By RahmXBot", "Safari", "3.0"]})
+
+store.bind(liaacans.ev)
+
+// anticall auto block
+liaacans.ws.on('CB:call', async (json) => {
+const callerId = json.content[0].attrs['call-creator']
+if (json.content[0].tag == 'offer') {
+let pa7rick = await liaacans.sendContact(callerId, global.owner)
+liaacans.sendMessage(callerId, { text: `Sistem otomatis block!\nJangan menelpon bot!\nSilahkan Hubungi Owner Untuk Dibuka !`}, { quoted : pa7rick })
+await sleep(8000)
+await liaacans.updateBlockStatus(callerId, "block")
+}
 })
 
 liaacans.ev.on('messages.upsert', async chatUpdate => {
 try {
 mek = chatUpdate.messages[0]
 if (!mek.message) return
-mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast') return
-if (!liaacans.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
-if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
-m = smsg(liaacans, m, store)
-require("./command/liaacans")(liaacans, m, chatUpdate, store)
+m.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (!liaacans.public && !m.key.fromMe && chatUpdate.type === 'notify') return
+if (m.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
+m = smsg(liaacans, mek, store)
+require('../command/liaacans')(liaacans, m, chatUpdate, store)
 } catch (err) {
-console.log(err)}
-})
+console.log(err)}})
 
-store.bind(liaacans.ev);
 liaacans.ev.on("connection.update", async up => {
 const { lastDisconnect, connection } = up;
 if (connection == "connecting") return
 if (connection){
 if (connection != "connecting") console.log("Connecting to jadibot..")
 }
-if (up.qr) await sendImage(m.chat, await qrcode.toDataURL(up.qr,{scale : 8}), 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik 3 di pojok kanan atas\n2. Klik Perangkat Tertaut\n3. Scan QR ini \nQR Expired dalam 30 detik', m)
+console.log(up)
+if (up.qr) await sendImage(from, await qrcode.toDataURL(up.qr,{scale : 8}), 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk WhatsApp Web\n3. Scan QR ini \nQR Expired dalam 30 detik', m)
 console.log(connection)
 if (connection == "open") {
 liaacans.id = liaacans.decodeJid(liaacans.user.id)
 liaacans.time = Date.now()
-global.listJadibot.push(liaacans)
-await m.reply(`*Connected to Whatsapp - Bot*\n\n*User :*\n _*× ID : ${liaacans.decodeJid(liaacans.user.id)}*_`)
-let user = `${liaacans.decodeJid(liaacans.user.id)}`
-let txt = `*Terdeteksi menumpang Jadibot*\n\n _× User : @${user.split("@")[0]}_`
-liaacans.sendMessage('6285821676621@s.whatsapp.net', {text: txt, mentions : [user]})
+global.conns.push(liaacans)
+user = `${liaacans.decodeJid(liaacans.user.id)}`
+txt = `*Terdeteksi User Jadibot*\n\n _× User : @${user.split("@")[0]}_`
+sendMessage(`62858213693245@s.whatsapp.net`,{text: txt, mentions : [user]})
 }
-
 if (connection === 'close') {
-let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-if (reason === DisconnectReason.badSession) { 
-console.log(`Bad Session File, Please Delete Session and Scan Again`); liaacans.logout(); }
-else if (reason === DisconnectReason.connectionClosed) { 
-console.log("Connection closed, reconnecting...."); startliaacans(); }
-else if (reason === DisconnectReason.connectionLost) { 
-console.log("Connection Lost from Server, reconnecting..."); startliaacans(); }
-else if (reason === DisconnectReason.connectionReplaced) { 
-console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); liaacans.logout(); }
-else if (reason === DisconnectReason.loggedOut) { 
-console.log(`Device Logged Out, Please Scan Again And Run.`); liaacans.logout(); }
-else if (reason === DisconnectReason.restartRequired) { 
-console.log("Restart Required, Restarting..."); startliaacans(); }
-else if (reason === DisconnectReason.timedOut) { 
-console.log("Connection TimedOut, Reconnecting..."); startliaacans(); }
-else liaacans.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? connectToWhatsApp() : ''
 }
 })
-
-liaacans.ev.on('creds.update', saveState)
 
 liaacans.decodeJid = (jid) => {
 if (!jid) return jid
@@ -94,18 +79,100 @@ return decode.user && decode.server && decode.user + '@' + decode.server || jid
 } else return jid
 }
 
+liaacans.ev.on('contacts.update', update => {
+for (let contact of update) {
+let id = liaacans.decodeJid(contact.id)
+if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
+}
+})
+
+liaacans.getName = (jid, withoutContact  = false) => {
+id = liaacans.decodeJid(jid)
+withoutContact = liaacans.withoutContact || withoutContact 
+let v
+if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
+v = store.contacts[id] || {}
+if (!(v.name || v.subject)) v = liaacans.groupMetadata(id) || {}
+resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
+})
+else v = id === '0@s.whatsapp.net' ? {
+id,
+name: 'WhatsApp'
+} : id === liaacans.decodeJid(liaacans.user.id) ?
+liaacans.user :
+(store.contacts[id] || {})
+return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
+}
+
+liaacans.public = true
+
+liaacans.setStatus = (status) => {
+liaacans.query({
+tag: 'iq',
+attrs: {
+to: '@s.whatsapp.net',
+type: 'set',
+xmlns: 'status',
+},
+content: [{
+tag: 'status',
+attrs: {},
+content: Buffer.from(status, 'utf-8')
+}]
+})
+return status
+}
+
+liaacans.serializeM = (m) => smsg(liaacans, m, store)
+
+liaacans.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+let quoted = message.msg ? message.msg : message
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(quoted, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+let type = await FileType.fromBuffer(buffer)
+trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+await fs.writeFileSync(trueFileName, buffer)
+return trueFileName
+}
+
+liaacans.downloadMediaMessage = async (message) => {
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(message, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+return buffer
+}
+
+liaacans.sendButtonText = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
+let buttonMessage = {
+text,
+footer,
+buttons,
+headerType: 2,
+...options
+}
+liaacans.sendMessage(jid, {text: text}, {
+         quoted,
+         ...options
+   })
+}
+
+liaacans.sendImage = async (jid, path, caption = '', quoted = '', options) => {
+let buffer = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+return await liaacans.sendMessage(jid, { image: buffer, caption: caption, ...options }, { quoted })
+}
+
 liaacans.sendText = (jid, text, quoted = '', options) => liaacans.sendMessage(jid, { text: text, ...options }, { quoted })
 
-/**
-* 
-* @param {*} jid 
-* @param {*} buttons 
-* @param {*} caption 
-* @param {*} footer 
-* @param {*} quoted 
-* @param {*} options 
-*/
-liaacans.sendButtonText = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
+liaacans.sendButMessage = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
 let buttonMessage = {
 text,
 footer,
@@ -117,20 +184,15 @@ liaacans.sendMessage(jid, buttonMessage, { quoted, ...options })
 }
 
 }
-startliaacans()
-} catch (e) {
-console.log(e)
-}
+connectToWhatsApp()
 }
 
-
-module.exports = { jadibot, listJadibot }
-
+module.exports = { jadibot, m }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(chalk.redBright(`Update ${__filename}`))
-delete require.cache[file]
-require(file)
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update ${__filename}`))
+    delete require.cache[file]
+    require(file)
 })
